2cd8ed22b98633456e718b074e56d4f53d664671
Ger Hobbelt <ger@hobbelt.com>
2012-11-09 20:47:53 +0100

    first small step to sync JS runtime with Java runtime: add BSD license header comment in every file, just like Java has it. ('compiled' == 'compressed' versions of the runtime will discard all comments, so it's no overhead in production environments...)

--- runtime/JavaScript/src/org/antlr/runtime/tree/CommonTree.js
+++ runtime/JavaScript/src/org/antlr/runtime/tree/CommonTree.js
@@ -1,3 +1,31 @@
+/*
+ [The "BSD license"]
+ Copyright (c) 2005-2009 Terence Parr
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+ 3. The name of the author may not be used to endorse or promote products
+     derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /** A tree node that is wrapper for a Token object.  After 3.0 release
  *  while building tree rewrite stuff, it became clear that computing
  *  parent and child index is very difficult and cumbersome.  Better to
@@ -5,20 +33,20 @@
  *  fields, it's easy to cut them out in your own BaseTree subclass.
  */
 org.antlr.runtime.tree.CommonTree = function(node) {
+    /** A single token is the payload */
+    this.token = null;
+
     /** What token indexes bracket all tokens associated with this node
      *  and below?
      */
     this.startIndex = -1;
     this.stopIndex = -1;
 
-    /** What index is this node in the child list? Range: 0..n-1 */
-    this.childIndex = -1;
-
     /** Who is the parent node of this node; if null, implies node is root */
     this.parent = null;
 
-    /** A single token is the payload */
-    this.token = null;
+    /** What index is this node in the child list? Range: 0..n-1 */
+    this.childIndex = -1;
 
     if (node instanceof org.antlr.runtime.tree.CommonTree) {
         org.antlr.runtime.tree.CommonTree.superclass.constructor.call(this, node);
