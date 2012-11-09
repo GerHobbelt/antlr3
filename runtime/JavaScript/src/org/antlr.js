09d35744a07beb47492df99fdf7a3444226cec27
Ger Hobbelt <ger@hobbelt.com>
2012-11-09 19:49:25 +0100

    typo fix in comment + indenting / whitespace

--- runtime/JavaScript/src/org/antlr.js
+++ runtime/JavaScript/src/org/antlr.js
@@ -38,7 +38,7 @@ org.antlr.global = (function() {
  * This fails because "long" is a future reserved word in ECMAScript
  *
  * @static
- * @param  {Array.<String>} arguments 1-n namespaces to create 
+ * @param  {Array.<String>} arguments 1-n namespaces to create
  * @return {Object}  A reference to the last namespace object created
  * @example
  * org.antlr.namespace("org.antlr.property.package");
@@ -70,10 +70,10 @@ org.antlr.env = org.antlr.env || {};
  * Do not fork for a browser if it can be avoided.  Use feature detection when
  * you can.  Use the user agent as a last resort.  org.antlr.env.ua stores a
  * version number for the browser engine, 0 otherwise.  This value may or may
- * not map to the version number of the browser using the engine.  The value is 
- * presented as a float so that it can easily be used for boolean evaluation 
- * as well as for looking for a particular range of versions.  Because of this, 
- * some of the granularity of the version info may be lost (e.g., Gecko 1.8.0.9 
+ * not map to the version number of the browser using the engine.  The value is
+ * presented as a float so that it can easily be used for boolean evaluation
+ * as well as for looking for a particular range of versions.  Because of this,
+ * some of the granularity of the version info may be lost (e.g., Gecko 1.8.0.9
  * reports 1.8).
  * @namespace org.antlr.env.ua
  */
@@ -95,7 +95,7 @@ org.antlr.env.ua = function() {
         opera:0,
 
         /**
-         * Gecko engine revision number.  Will evaluate to 1 if Gecko 
+         * Gecko engine revision number.  Will evaluate to 1 if Gecko
          * is detected but the revision could not be found. Other browsers
          * will be 0.  Example: 1.8
          * <pre>
@@ -110,10 +110,10 @@ org.antlr.env.ua = function() {
         gecko:0,
 
         /**
-         * AppleWebKit version.  KHTML browsers that are not WebKit browsers 
+         * AppleWebKit version.  KHTML browsers that are not WebKit browsers
          * will evaluate to 1, other browsers 0.  Example: 418.9.1
          * <pre>
-         * Safari 1.3.2 (312.6): 312.8.1 <-- Reports 312.8 -- currently the 
+         * Safari 1.3.2 (312.6): 312.8.1 <-- Reports 312.8 -- currently the
          *                                   latest available for Mac OSX 10.3.
          * Safari 2.0.2:         416     <-- hasOwnProperty introduced
          * Safari 2.0.4:         418     <-- preventDefault fixed
@@ -123,15 +123,15 @@ org.antlr.env.ua = function() {
          *                                   updated, but not updated
          *                                   to the latest patch.
          * Webkit 212 nightly:   522+    <-- Safari 3.0 precursor (with native SVG
-         *                                   and many major issues fixed).  
+         *                                   and many major issues fixed).
          * 3.x yahoo.com, flickr:422     <-- Safari 3.x hacks the user agent
-         *                                   string when hitting yahoo.com and 
+         *                                   string when hitting yahoo.com and
          *                                   flickr.com.
          * Safari 3.0.4 (523.12):523.12  <-- First Tiger release - automatic update
          *                                   from 2.x via the 10.4.11 OS patch
          * Webkit nightly 1/2008:525+    <-- Supports DOMContentLoaded event.
          *                                   yahoo.com user agent hack removed.
-         *                                   
+         *
          * </pre>
          * http://developer.apple.com/internet/safari/uamatrix.html
          * @property webkit
@@ -143,8 +143,8 @@ org.antlr.env.ua = function() {
          * The mobile property will be set to a string containing any relevant
          * user agent information when a modern mobile browser is detected.
          * Currently limited to Safari on the iPhone/iPod Touch, Nokia N-series
-         * devices with the WebKit-based browser, and Opera Mini.  
-         * @property mobile 
+         * devices with the WebKit-based browser, and Opera Mini.
+         * @property mobile
          * @type string
          */
         mobile: null,
@@ -183,17 +183,17 @@ org.antlr.env.ua = function() {
             // Mobile browser check
             if (/ Mobile\//.test(ua)) {
                 o.mobile = "Apple"; // iPhone or iPod Touch
-        } else {
-            m=ua.match(/NokiaN[^\/]*/);
-            if (m) {
-                o.mobile = m[0]; // Nokia N-series, ex: NokiaN95
+            } else {
+                m=ua.match(/NokiaN[^\/]*/);
+                if (m) {
+                    o.mobile = m[0]; // Nokia N-series, ex: NokiaN95
+                }
             }
-        }
 
-        m=ua.match(/AdobeAIR\/([^\s]*)/);
-        if (m) {
-            o.air = m[0]; // Adobe AIR 1.0 or better
-        }
+            m=ua.match(/AdobeAIR\/([^\s]*)/);
+            if (m) {
+                o.air = m[0]; // Adobe AIR 1.0 or better
+            }
 
         }
 
@@ -232,7 +232,7 @@ org.antlr.env.ua = function() {
         }
     } catch(e) {}
 
-    
+
     return o;
 }();
 
@@ -249,13 +249,13 @@ org.antlr.env.ua = function() {
 org.antlr.namespace("org.antlr.runtime.tree");
 
 /**
- * Provides the language utilites and extensions used by the library
+ * Provides the language utilities and extensions used by the library
  * @namespace org.antlr.lang
  */
 org.antlr.lang = org.antlr.lang || /** @lends org.antlr.lang */ {
     /**
      * Determines whether or not the provided object is an array.
-     * Testing typeof/instanceof/constructor of arrays across frame 
+     * Testing typeof/instanceof/constructor of arrays across frame
      * boundaries isn't possible in Safari unless you have a reference
      * to the other frame to test against its Array prototype.  To
      * handle this case, we test well-known array properties instead.
@@ -263,7 +263,7 @@ org.antlr.lang = org.antlr.lang || /** @lends org.antlr.lang */ {
      * @param {any} o The object being testing
      * @return {boolean} the result
      */
-    isArray: function(o) { 
+    isArray: function(o) {
         if (o) {
            var l = org.antlr.lang;
            return l.isNumber(o.length) && l.isFunction(o.splice);
@@ -279,7 +279,7 @@ org.antlr.lang = org.antlr.lang || /** @lends org.antlr.lang */ {
     isBoolean: function(o) {
         return typeof o === 'boolean';
     },
-    
+
     /**
      * Determines whether or not the provided object is a function
      * @param {any} o The object being testing
@@ -288,7 +288,7 @@ org.antlr.lang = org.antlr.lang || /** @lends org.antlr.lang */ {
     isFunction: function(o) {
         return typeof o === 'function';
     },
-        
+
     /**
      * Determines whether or not the provided object is null
      * @param {any} o The object being testing
@@ -297,7 +297,7 @@ org.antlr.lang = org.antlr.lang || /** @lends org.antlr.lang */ {
     isNull: function(o) {
         return o === null;
     },
-        
+
     /**
      * Determines whether or not the provided object is a legal number
      * @param {any} o The object being testing
@@ -306,17 +306,17 @@ org.antlr.lang = org.antlr.lang || /** @lends org.antlr.lang */ {
     isNumber: function(o) {
         return typeof o === 'number' && isFinite(o);
     },
-      
+
     /**
      * Determines whether or not the provided object is of type object
      * or function
      * @param {any} o The object being testing
      * @return {boolean} the result
-     */  
+     */
     isObject: function(o) {
-return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
+        return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
     },
-        
+
     /**
      * Determines whether or not the provided object is a string
      * @param {any} o The object being testing
@@ -325,7 +325,7 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
     isString: function(o) {
         return typeof o === 'string';
     },
-        
+
     /**
      * Determines whether or not the provided object is undefined
      * @param {any} o The object being testing
@@ -334,11 +334,11 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
     isUndefined: function(o) {
         return typeof o === 'undefined';
     },
-    
+
     /**
      * IE will not enumerate native functions in a derived object even if the
-     * function was overridden.  This is a workaround for specific functions 
-     * we care about on the Object prototype. 
+     * function was overridden.  This is a workaround for specific functions
+     * we care about on the Object prototype.
      * @param {Function} r  the object to receive the augmentation
      * @param {Function} s  the object that supplies the properties to augment
      * @private
@@ -354,7 +354,7 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
             }
         }
     },
-       
+
     /**
      * Utility to set up the prototype, constructor and superclass properties to
      * support an inheritance strategy that can chain constructors and methods.
@@ -366,7 +366,7 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
      * @param {Function} superc the object to inherit
      * @param {Object} [overrides]  additional properties/methods to add to the
      *                              subclass prototype.  These will override the
-     *                              matching items obtained from the superclass 
+     *                              matching items obtained from the superclass
      *                              if present.
      */
     extend: function(subc, superc, overrides) {
@@ -382,7 +382,7 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
         if (superc.prototype.constructor == Object.prototype.constructor) {
             superc.prototype.constructor=superc;
         }
-    
+
         if (overrides) {
             for (var i in overrides) {
                 subc.prototype[i]=overrides[i];
@@ -391,14 +391,14 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
             org.antlr.lang._IEEnumFix(subc.prototype, overrides);
         }
     },
-   
+
     /**
      * Applies all properties in the supplier to the receiver if the
-     * receiver does not have these properties yet.  Optionally, one or 
-     * more methods/properties can be specified (as additional 
-     * parameters).  This option will overwrite the property if receiver 
-     * has it already.  If true is passed as the third parameter, all 
-     * properties will be applied and _will_ overwrite properties in 
+     * receiver does not have these properties yet.  Optionally, one or
+     * more methods/properties can be specified (as additional
+     * parameters).  This option will overwrite the property if receiver
+     * has it already.  If true is passed as the third parameter, all
+     * properties will be applied and _will_ overwrite properties in
      * the receiver.
      *
      * @param {Function} r  the object to receive the augmentation
@@ -421,26 +421,26 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
                 r[a[i]] = s[a[i]];
             }
         } else { // take everything, overwriting only if the third parameter is true
-            for (p in s) { 
+            for (p in s) {
                 if (override || !r[p]) {
                     r[p] = s[p];
                 }
             }
-            
+
             org.antlr.lang._IEEnumFix(r, s);
         }
     },
- 
+
     /**
      * Same as org.antlr.lang.augmentObject, except it only applies prototype properties
      * @see org.antlr.lang.augmentObject
      * @param {Function} r  the object to receive the augmentation
      * @param {Function} s  the object that supplies the properties to augment
-     * @param {Array.<String>|boolean}  [arguments] zero or more properties methods 
-     *        to augment the receiver with.  If none specified, everything 
-     *        in the supplier will be used unless it would overwrite an existing 
-     *        property in the receiver.  if true is specified as the third 
-     *        parameter, all properties will be applied and will overwrite an 
+     * @param {Array.<String>|boolean}  [arguments] zero or more properties methods
+     *        to augment the receiver with.  If none specified, everything
+     *        in the supplier will be used unless it would overwrite an existing
+     *        property in the receiver.  if true is specified as the third
+     *        parameter, all properties will be applied and will overwrite an
      *        existing property in the receiver
      */
     augmentProto: function(r, s) {
@@ -472,14 +472,14 @@ return (o && (typeof o === 'object' || org.antlr.lang.isFunction(o))) || false;
 
     /**
      * A convenience method for detecting a legitimate non-null value.
-     * Returns false for null/undefined/NaN, true for other values, 
+     * Returns false for null/undefined/NaN, true for other values,
      * including 0/false/''
      * @param o {any} the item to test
      * @return {boolean} true if it is not null/undefined/NaN || false
      */
     isValue: function(o) {
         var l = org.antlr.lang;
-return (l.isObject(o) || l.isString(o) || l.isNumber(o) || l.isBoolean(o));
+        return (l.isObject(o) || l.isString(o) || l.isNumber(o) || l.isBoolean(o));
     },
 
     /** @namespace org.antlr.lang.array Array convenience methods. */
